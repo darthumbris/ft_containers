9803df3cc06596ae633c40264252141606496277
Siebe Hoogenboom <shoogenb@student.codam.nl>
2022-09-23 15:33:46 +0200

    cleaned up the testing script

--- test_container.sh
+++ test_container.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    test_container.sh                                  :+:    :+:             #
+#                                                      +:+                     #
+#    By: shoogenb <shoogenb@student.codam.nl>         +#+                      #
+#                                                    +#+                       #
+#    Created: 2022/09/23 15:26:12 by shoogenb      #+#    #+#                  #
+#    Updated: 2022/09/23 15:33:00 by shoogenb      ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
+# Seed can be changed to test with other random values
 seed=42
 
 yellow="\033[33m"
@@ -8,94 +21,39 @@ red="\033[31m"
 blue="\033[34m"
 reset="\033[0m"
 
+container_test_function(){
+    container=$1
+
+    {
+    make fclean; 
+    make STD_MODE=0 -j4;
+    } &> /dev/null
+    echo -e "${yellow} Test - FT_${container} ${reset}";
+    ./ft_containers $seed $container;
+    echo -e "${blue} [Done] ${reset}";
+
+    {
+        make fclean;
+        make STD_MODE=1 -j4;
+    } &> /dev/null
+    echo -e "${yellow} Test - STD_${container} ${reset}";
+    ./ft_containers $seed $container;
+    echo -e "${blue} [Done] ${reset}";
+    diff -u test/out_ft.log test/out_std.log > test/diff.log;
+    if [ -s test/diff.log ]; then
+        echo -e "${red} Diff found check test/diff.log ${reset}";
+        exit 1
+    else
+        echo -e "${green} no diff found ${reset}";
+    fi
+}
+
 echo "making test directory for putting output of test";
 mkdir -p -v test;
-container=vector
-{
-    make fclean; 
-    make STD_MODE=0 -j4;
-} &> /dev/null
 
-echo -e "${yellow} Test - FT_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-{
-    make fclean;
-    make STD_MODE=1 -j4;
-} &> /dev/null
+container_test_function "vector"
+container_test_function "map"
+container_test_function "stack"
+container_test_function "set"
 
-echo -e "${yellow} Test - STD_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-diff -u test/out_ft.log test/out_std.log > test/diff.log;
-if [ -s test/diff.log ]; then
-    echo -e "${red} Diff found check test/diff.log ${reset}";
-else
-    echo -e "${green} no diff found ${reset}";
-fi
-
-container=map
-{
-    make fclean; 
-    make STD_MODE=0 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - FT_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-{
-    make fclean;
-    make STD_MODE=1 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - STD_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-diff -u test/out_ft.log test/out_std.log > test/diff.log;
-if [ -s test/diff.log ]; then
-    echo -e "${red} Diff found check test/diff.log ${reset}";
-else
-    echo -e "${green} no diff found ${reset}";
-fi
-
-container=stack
-{
-    make fclean; 
-    make STD_MODE=0 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - FT_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-{
-    make fclean;
-    make STD_MODE=1 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - STD_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-diff -u test/out_ft.log test/out_std.log > test/diff.log;
-if [ -s test/diff.log ]; then
-    echo -e "${red} Diff found check test/diff.log ${reset}";
-else
-    echo -e "${green} no diff found ${reset}";
-fi
-
-container=set
-{
-    make fclean; 
-    make STD_MODE=0 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - FT_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-{
-    make fclean;
-    make STD_MODE=1 -j4;
-} &> /dev/null
-echo -e "${yellow} Test - STD_${container} ${reset}";
-./ft_containers $seed $container;
-echo -e "${blue} [Done] ${reset}";
-diff -u test/out_ft.log test/out_std.log > test/diff.log;
-if [ -s test/diff.log ]; then
-    echo -e "${red} Diff found check test/diff.log ${reset}";
-else
-    echo -e "${green} no diff found ${reset}";
-fi
+echo -e "${green} all test passed! ${reset}";
